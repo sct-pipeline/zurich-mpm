a345569e566b7fda85ac977eab1124f7d65f4371
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-09-05 14:38:59 -0400

    process_data.sh: Add multiple echoes

--- process_data.sh
+++ process_data.sh
@@ -40,7 +40,7 @@ label_if_does_not_exist(){
     rsync -avzh "${PATH_SEGMANUAL}/${file}_labels-manual.nii" ${FILELABEL}.nii
   else
     # Generate labeled segmentation
-    sct_label_vertebrae -i ${file}.nii -s ${file_seg}.nii -c t2 -qc ${PATH_QC} -qc-subject ${SUBJECT}
+    sct_label_vertebrae -i ${file}.nii -s ${file_seg}.nii -c t1 -qc ${PATH_QC} -qc-subject ${SUBJECT}
     # Create labels in the cord at C2 and C3 mid-vertebral levels
     sct_label_utils -i ${file_seg}_labeled.nii -vert-body 2,3 -o ${FILELABEL}.nii
   fi
@@ -76,21 +76,31 @@ cp -r $PATH_DATA/$SUBJECT .
 # Go to data folder
 cd $SUBJECT/anat/
 # Setup file names
+file_t1w=${SUBJECT}_echo-MEAN_T1w
 file_r1=${SUBJECT}_RFSC_R1
 file_r2s=${SUBJECT}_RFSC_R2s_OLS
 file_pd=${SUBJECT}_RFSC_PD
 file_mt=${SUBJECT}_RFSC_MT
+# Average all echoes of the T1w images
+sct_image -i ${SUBJECT}_echo-?_T1w.nii -concat t -o ${SUBJECT}_echo-ALL_T1w.nii
+sct_maths -i ${SUBJECT}_echo-ALL_T1w.nii -mean t -o $file_t1w.nii
+# Copy header from correct header (because q/sform are different)
+sct_image -i $file_r1.nii -copy-header $file_t1w.nii
 # Segment spinal cord (only if it does not exist)
-segment_if_does_not_exist $file_r1 "t1"
+segment_if_does_not_exist $file_t1w "t1"
 file_seg=$FILESEG
 # Create labels in the cord at C2 and C3 mid-vertebral levels (only if it does not exist)
-label_if_does_not_exist $file_pd $file_seg
+label_if_does_not_exist $file_t1w $file_seg
 file_label=$FILELABEL
 # Register to template
 # Note: for such a small rostro-caudal distance, we can use -ref subject
-sct_register_to_template -i $file_r1.nii -s $file_seg.nii -l $file_label.nii -c t1 -ref subject -param step=1,type=seg,algo=centermassrot:step=2,type=im,algo=syn,iter=5,slicewise=1,metric=CC,smooth=0 -qc $PATH_QC
+sct_register_to_template -i $file_t1w.nii -s $file_seg.nii -l $file_label.nii -c t1 -ref subject -param step=1,type=seg,algo=centermassrot:step=2,type=im,algo=syn,iter=5,slicewise=1,metric=CC,smooth=0 -qc $PATH_QC
 # Warp template
-sct_warp_template -d $file_r1.nii -w warp_template2anat.nii.gz -qc $PATH_QC
+sct_warp_template -d $file_t1w.nii -w warp_template2anat.nii.gz -qc $PATH_QC
+# Check registration across metrics
+sct_qc -i $file_r2s.nii -s $file_seg.nii -p sct_propseg -qc $PATH_QC
+sct_qc -i $file_pd.nii -s $file_seg.nii -p sct_propseg -qc $PATH_QC
+sct_qc -i $file_mt.nii -s $file_seg.nii -p sct_propseg -qc $PATH_QC
 # Compute average CSA between C2 and C3 levels (append across subjects)
 sct_process_segmentation -i $file_seg.nii -vert 2:3 -o $PATH_RESULTS/CSA.csv -append 1 -qc $PATH_QC
 # Compute average MPM metrics in the spinal cord white matter between C2 and C3 levels (append across subjects)
